d2a7ed8a93d904869999b4d3084109721a9ee899
Zoltan HERPAI <wigyori@uid0.hu>
2023-08-31 23:35:27 +0200

    sunxi: t113: add eMMC image generation support
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -266,7 +266,9 @@ define Device/myir_myd-yt113x
   DEVICE_MODEL := MYD-YT113X
   DEVICE_PACKAGES := kmod-rtc-sunxi kmod-eeprom-at24 kmod-gpio-pca953x kmod-rtc-rx8025
   SOC := sun8i-t113s
+  IMAGES += emmc.img.gz
   IMAGE/sdcard.img.gz := sunxi-sdcard | append-metadata | gzip
+  IMAGE/emmc.img.gz := boot-scr-emmc | sunxi-emmc | append-metadata | gzip
 endef
 TARGET_DEVICES += myir_myd-yt113x
 
@@ -283,6 +285,8 @@ define Device/rongpin_rp-t113
   DEVICE_MODEL := RP-T113
   DEVICE_PACKAGES:=kmod-rtc-sunxi
   SOC := sun8i-t113s
+  IMAGES += emmc.img.gz
   IMAGE/sdcard.img.gz := sunxi-sdcard | append-metadata | gzip
+  IMAGE/emmc.img.gz := boot-scr-emmc | sunxi-emmc | append-metadata | gzip
 endef
 TARGET_DEVICES += rongpin_rp-t113
