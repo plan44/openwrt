1742e4960db5aa4acc829a005f04e84e695cd1be
Lukas Zeller <luz@plan44.ch>
2025-09-14 18:12:53 +0200

    target/linux/rockchip/cortexta7: platform.sh: support sysupgrade for nand targets (pico pro max, p44-xx-pico-max)

--- target/linux/rockchip/cortexa7/base-files/lib/upgrade/platform.sh
+++ target/linux/rockchip/cortexa7/base-files/lib/upgrade/platform.sh
@@ -17,12 +17,12 @@ platform_do_upgrade() {
 	rockchip,rv1106g-luckfox-pico-86panel-w)
 	  # set BOOTDEV_MAJOR and BOOTDEV_MINOR env vars
 		export_bootdevice
-		# set "rootdev" env var (can by any var name as needed)
+		# set "rootdev" env var (can be any var name as needed)
 		# to the device name of 0=root, 1,2,…=partition1,2,
 		# Note: this looks up the device name (not path!) based on BOOTDEV_MAJOR + BOOTDEV_MINOR+<given number>
 		export_partdevice rootdev 0
 		CI_ROOTDEV="$rootdev"
-		# CI_xx are partition names, known by the kernel via /sys/block/$rootdev/${rootdev}p*/uevent
+		# CI_xx are partition names, known by the kernel via /sys/block/$rootdev/${rootdev}p*/uevent PARTNAME
 		# when these are set (and EMMC_xx_DEV are NOT set), emmc_do_upgrade uses find_mmc_part() to
 		# define EMMC_xx_DEV accordingly.
 		CI_KERNPART="boot"
@@ -30,12 +30,19 @@ platform_do_upgrade() {
 		# alternatively, EMMC_xx_DEV can be specified directly instead of by name with CI_xx
 		# but that requires a fixed partition numbering schema, which is dangerous
 		## This is how the partition numbers are for luckfox boards at this time
-		#export_partdevice rootfspart 4
+		#export_partdevice kernpart 4
 		#export_partdevice rootfspart 7
 		#export EMMC_KERN_DEV="/dev/$kernpart"
 		#export EMMC_ROOT_DEV="/dev/$rootfspart"
 		# now perform the update
 		emmc_do_upgrade "$1"
+		;;
+	luckfox,rv1106-luckfox-pico-max|\
+	plan44,p44-xx-pico-max)
+		# CI_xx are mtd partition names, known by the kernel via /sys/block/mtdblockX/device/name
+		CI_KERNPART="boot"
+		CI_ROOTPART="rootfs"
+		nand_do_upgrade "$1"
 		;;
 	*)
 		default_do_upgrade "$1"
@@ -45,16 +52,20 @@ platform_do_upgrade() {
 
 platform_check_image() {
 	local magic="$(get_magic_long "$1")"
+	local board=$(board_name)
 
 	[ "$#" -gt 1 ] && return 1
 
-	case "$(board_name)" in
+	case "$board" in
 	rockchip,rv1106g-luckfox-pico-86panel-w)
-	  #test for fit image, which is NOT what we have, we use tar
-    # [ "$magic" != "d00dfeed" ] && {
-    # 	echo "Invalid image type."
-    # 	return 1
-    # }
+		# NAND check is ok-ish here, as we use a tar file
+		nand_do_platform_check "$board" "$1"
+		return $?
+		;;
+	luckfox,rv1106-luckfox-pico-max|\
+	plan44,p44-xx-pico-max)
+		nand_do_platform_check "$board" "$1"
+		return $?
 		;;
 	*)
 		echo "Sysupgrade is not supported on your board yet."
